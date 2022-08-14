a484e19df994c747d41b99ed97ca634f8b521a43
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2022-08-14 15:56:50 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
-function App() {
+class Counter extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div id='main'>
+  }
+
+
+
+  render() {
+    const { value, onIncrement, onDecrement } = this.props
+    return (
+      <p data-testid='counter'>
+        Clicked: {value} times
+        {' '}
+        <button onClick={onIncrement}>
+          +
+        </button>
+        {' '}
+        <button onClick={onDecrement}>
+          -
+          </button>
       
-    </div>
-  );
+      </p>
+    )
+  }
 }
 
-export default App;
+Counter.propTypes = {
+  value: PropTypes.number.isRequired,
+  onIncrement: PropTypes.func.isRequired,
+  onDecrement: PropTypes.func.isRequired
+}
+
+export default Counter;
