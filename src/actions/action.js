6d37871ab326553ca8469c63eb3d485387d14b25
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2022-08-25 18:08:05 +0530

    Update action.js

---
+++ src/actions/action.js
@@ -0,0 +1,11 @@
+// export const increment = () => {
+//       return {type: 'INCREMENT'}
+//   }
+// export const decrement = () => {
+//     return {type: 'DECREMENT'}
+//   }
+
+
+// Actions
+const inc = () => ({ type: 'INCREMENT' });
+const dec = () => ({ type: 'DECREMENT' });
