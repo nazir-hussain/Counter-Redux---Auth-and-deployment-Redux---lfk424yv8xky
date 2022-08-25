5930089ff11c53d74e9730116a466403d9047568
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2022-08-25 18:09:04 +0530

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,20 +1,54 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import Counter from './Components/App'
-import counter from './reducers'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import { createStore } from 'redux'
+// import Counter from './Components/App'
+// import counter from './reducers'
 
-const store = createStore(counter)
-const rootEl = document.getElementById('root')
+// const store = createStore(counter)
+// const rootEl = document.getElementById('root')
 
-const render = () => ReactDOM.render(
-  <Counter
-    value={store.getState()}
-    onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
-    onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
-  />,
-  rootEl
-)
+// const render = () => ReactDOM.render(
+//   <Counter
+//     value={store.getState()}
+//     onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+//     onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+//   />,
+//   rootEl
+// )
 
-render()
-store.subscribe(render)
+// render()
+// store.subscribe(render)
+
+
+
+
+
+
+
+
+
+// const countReducer = (state = 0, action) =>{
+//    switch(action.type){ 
+//     case 'INCREMENT':
+//        return state + 1;
+    
+//     case 'DECREMENT':
+//        return state - 1;
+    
+//    default:
+//       return state;
+//   }
+// };
+//   export default countReducer;
+
+const reducer = (state = initialState, action) => {
+	switch(action.type) {
+  	case 'INCREMENT':
+    	return Object.assign({}, state, { count: state.count + 1 });
+  	case 'DECREMENT':
+    	return Object.assign({}, state, { count: state.count - 1 });
+  }
+  return state;
+}
+
+export default reducer;
