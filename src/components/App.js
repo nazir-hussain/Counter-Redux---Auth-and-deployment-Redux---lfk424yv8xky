ce04df2415b5f7a439d5bcdb82aa9b7fedf915ca
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2022-08-25 18:07:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,79 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+// import React, { Component } from 'react'
+// import PropTypes from 'prop-types'
 
-class Counter extends Component {
-  constructor(props) {
-    super(props);
+// class Counter extends Component {
+//   constructor(props) {
+//     super(props);
 
-  }
+//   }
 
 
 
-  render() {
-    const { value, onIncrement, onDecrement } = this.props
-    return (
-      <p data-testid='counter'>
-        Clicked: {value} times
-        {' '}
-        <button onClick={onIncrement}>
-          +
-        </button>
-        {' '}
-        <button onClick={onDecrement}>
-          -
-          </button>
+//   render() {
+//     const { value, onIncrement, onDecrement } = this.props
+//     return (
+//       <p data-testid='counter'>
+//         Clicked: {value} times
+//         {' '}
+//         <button onClick={onIncrement}>
+//           +
+//         </button>
+//         {' '}
+//         <button onClick={onDecrement}>
+//           -
+//           </button>
       
-      </p>
-    )
-  }
+//       </p>
+//     )
+//   }
+// }
+
+// Counter.propTypes = {
+//   value: PropTypes.number.isRequired,
+//   onIncrement: PropTypes.func.isRequired,
+//   onDecrement: PropTypes.func.isRequired
+// }
+
+// export default Counter;
+
+
+
+
+
+
+
+import React from 'react';
+import '../styles/App.css';
+import {createStore} from 'redux';
+import reducer from '../reducers/index';
+import {Provider,connect} from 'react-redux';
+import {inc,dec} from '../actions/action';
+function App() {
+
+  const store = createStore(reducer);
+  
+
+  return (
+    <div id='main'>
+      <Provider store={store}>
+        <Counter/>
+      </Provider>
+    </div> 
+  );
 }
 
-Counter.propTypes = {
-  value: PropTypes.number.isRequired,
-  onIncrement: PropTypes.func.isRequired,
-  onDecrement: PropTypes.func.isRequired
-}
+export default App;
 
-export default Counter;
+
+const mapCount = state => ({ count: state.count });
+
+
+
+// connected component
+const Counter = connect(mapCount, { inc, dec })(props => (
+	<div >
+         <button onClick={props.inc} >+</button> 
+         <span data-testid='counter'> Count: {props.count}</span>
+        <button onClick={props.dec}>-</button>
+        </div>
+));
